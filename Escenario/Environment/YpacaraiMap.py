fbe945bfd8017ba692fc143b316b8829e1e5cc27
Samuel <derpberk@gmail.com>
2020-07-13 22:44:03 +0100

    Arreglado bug con el método self.metricas()
    Ahora se cambia el atributo mediante el metodo YpacaraiEnvironement.set_test_mode(True)

--- Escenario/Environment/YpacaraiMap.py
+++ Escenario/Environment/YpacaraiMap.py
@@ -16,12 +16,23 @@ class Environment:
     
     def __init__(self):
         
-        # Cargamos el mapa #
+        # Cargamos el mapa y creamos las matrices de estado#
         self.map = genfromtxt('./YpacaraiMap.csv', delimiter=',',dtype = int)
         self.S = np.ones(self.map.shape)
         self.R_abs = np.ones(self.map.shape)*255
         self.visited = np.zeros(self.map.shape)
         
+        # Mapas para las metricas #
+        self.visited_M = np.zeros(shape = self.map.shape)
+        self.waiting_M = np.ones(shape = self.map.shape)
+        self.mean_time_M = np.zeros(shape = self.map.shape)
+        
+        # El test mode pone el mapa en modo test y no para entrenar #
+        # Se realizan métricas extras no necesarias en el entrenamiento y que podrían
+        # realentizarlo #
+        self.test_mode = False
+        
+        # Seleccionamos un punto aleatorio de comienzo #
         posible_x, posible_y = np.nonzero(self.map)
         
         init_cell_index = np.random.randint(0,posible_x.size)
@@ -32,17 +43,28 @@ class Environment:
         self.agent_pos_ant = self.agent_start_pos
         
         # Marcamos el inicio como visitado #
+        
         self.visited[self.agent_pos[0]][self.agent_pos[1]] = 255
         self.S[self.agent_pos[0]][self.agent_pos[1]] = 0
         self.R_abs[self.agent_pos[0]][self.agent_pos[1]] -= 50
         
     def reset(self):
-        # Reseteamos todo
+        # Reseteamos las matrices de estado #
         
         self.S = np.ones(self.map.shape)
         self.R_abs = np.ones(self.map.shape)*255
         self.visited = np.zeros(self.map.shape)
         
+        # Mapas para las metricas #
+        self.visited_M = np.zeros(shape = self.map.shape)
+        self.waiting_M = np.ones(shape = self.map.shape)
+        self.mean_time_M = np.zeros(shape = self.map.shape)
+        
+        # El test mode pone el mapa en modo test y no para entrenar #
+        # Se realizan métricas extras no necesarias en el entrenamiento y que podrían
+        # realentizarlo #
+        
+        # Seleccionamos un punto aleatorio de comienzo #
         posible_x, posible_y = np.nonzero(self.map)
         
         init_cell_index = np.random.randint(0,posible_x.size)
@@ -112,7 +134,7 @@ class Environment:
         
         reward = rho_next - rho_act
         # Ojito que aquí decidimos cuánto penalizamos visitar una ilegal una anterior o una nueva #
-        reward = (1-ilegal)*((1.507/255)*(reward-255)+1.5) - ilegal*5
+        reward = (1-ilegal)*((1.507/255)*(reward-255)+1) - ilegal*(0.5)
         
         # Actualizamos la matriz S #
         
@@ -133,9 +155,23 @@ class Environment:
         obs['importance_map'] = self.S*self.R_abs
         obs['position'] = self.agent_pos
         
-        # Nunca acabamos #
-        
+        # Nunca acabamos - Non episodic #
         done = 0
+        
+        # PARA TEST_MODE ACTIVADO #
+        if(self.test_mode == True):
+            # Aumentamos una visita en la celda correspondiente #
+            self.visited_M[self.agent_pos[0]][self.agent_pos[1]] += 1
+            # Tomamos el tiempo que se ha tardado en visitar la celda esa vez #
+            T = self.waiting_M[self.agent_pos[0]][self.agent_pos[1]]
+            # Aumentamos el tiempo de todas las celdas menos la que acabamos de visitar #
+            self.waiting_M +=  1
+            self.waiting_M[self.agent_pos[0]][self.agent_pos[1]] = 1
+            # Acumulamos el tiempo de visita de la celda #
+            self.mean_time_M[self.agent_pos[0]][self.agent_pos[1]] += T
+            
+        else:
+            pass
         
         return obs, reward, done, ilegal
     
@@ -171,6 +207,65 @@ class Environment:
     def action_space_sample(self):
         
         return np.random.randint(0,8)
+
+    def set_test_mode(self,test_mode = False):
+        self.test_mode = test_mode
+
+        if(test_mode == True):
+            print("Modo test activado")
+        else:
+            print("Modo test desactivado")
+
+    
+    
+    # Metodo que devuelve las metricas #
+    def metrics(self):
+        
+        if self.test_mode == False:
+            print("El modo test no se ha activado. Esta llamada no tendrá efecto")
+            return -1
+        
+        # Metrica de coverage #
+        
+        num_celdas_cubiertas = np.count_nonzero(self.visited)
+        num_celdas_visitables = np.count_nonzero(self.map)
+        
+        coverage = num_celdas_cubiertas/num_celdas_visitables
+        
+        # Metrica de la frecuencia media de visitas #
+        
+        V = []
+        # obtenemos el vector de tiempo de espera de cada visita#
+        T_mean_M = self.mean_time_M/self.visited_M
+        
+        for i in range(0,self.map.shape[0]):
+            for j in range(0,self.map.shape[1]):
+                
+                if(self.map[i][j] == 1 and T_mean_M[i][j] != np.inf and T_mean_M[i][j] > 0):
+                    V.append(T_mean_M[i][j])
+        
+        
+        # Calculamos la media #                    
+        mean = np.mean(V)
+        
+        # La desviación tipica #
+        std = np.std(V)
+        
+        metricsD = {}
+        metricsD['coverage'] = coverage
+        metricsD['mean'] = mean
+        metricsD['std'] = std
+        metricsD['time_matrix'] = T_mean_M
+        metricsD['visited_acc_matrix'] = self.visited_M
+        metricsD['V'] = V
+        
+        return metricsD
+    
+        
+        
+        
+        
+        
                     
                 
         
